7b2ff0ccc8c83a0ed4fbe7f36d045d67bcca3657
laboratorio <laboratorio@UPOAULA10218>
2025-01-20 07:10:32 -0500

    Agregar archivos de proyecto.

---
+++ GavilanesAplicacionPais/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GavilanesAplicacionPais
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GavilanesAplicacionPais/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GavilanesAplicacionPais
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
