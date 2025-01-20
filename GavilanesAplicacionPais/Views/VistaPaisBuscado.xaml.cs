edf0b6006977da568e72856cddecd4b0cd6b1b1d
JohaoG10 <johao.gavilanes@udla.edu.ec>
2025-01-20 08:19:39 -0500

    vista Principal

--- GavilanesAplicacionPais/Views/VistaPaisBuscado.xaml.cs
+++ GavilanesAplicacionPais/Views/VistaPaisBuscado.xaml.cs
@@ -3,22 +3,21 @@ using GavilanesAplicacionPais.Services;
 using Microsoft.Maui.Controls;
 using System.Collections.Generic;
 
-namespace GavilanesAplicacionPais
-{
-    public partial class VistaPaisBuscado : ContentPage
-    {
-        private readonly ServicioBaseDeDatos _servicioBaseDeDatos;
+namespace GavilanesAplicacionPais.Views;
 
-        public VistaPaisBuscado()
-        {
-            InitializeComponent(); 
-            _servicioBaseDeDatos = new ServicioBaseDeDatos();
-            CargarPaises();
-        }
-        private async void CargarPaises()
-        {
-            var listaPaises = await _servicioBaseDeDatos.ObtenerPaisesAsync();
-            listViewPaises.ItemsSource = listaPaises; 
-        }
+public partial class VistaPaisBuscado : ContentPage
+{
+    private readonly ServicioBaseDeDatos _servicioBaseDeDatos;
+
+    public VistaPaisBuscado()
+    {
+        InitializeComponent(); 
+        _servicioBaseDeDatos = new ServicioBaseDeDatos();
+        CargarPaises();
+    }
+    private async void CargarPaises()
+    {
+        var listaPaisesConsultados = await _servicioBaseDeDatos.ObtenerPaisesAsync();
+        listViewPaises.ItemsSource = listaPaisesConsultados; 
     }
 }
